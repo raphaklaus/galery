8624f3a1a8c74daa9e4a4902bea4684fe761e6f0
Raphael D. Pinheiro <raphaklaus@gmail.com>
2014-08-23 11:50:10 -0700

    Creating the Slider...

--- app/controllers/fotos_controller.rb
+++ app/controllers/fotos_controller.rb
@@ -28,7 +28,7 @@ class FotosController < ApplicationController
 
     respond_to do |format|
       if @foto.save
-        format.html { redirect_to @foto, notice: 'Foto was successfully created.' }
+        format.html { redirect_to @foto, notice: 'A Foto foi criada corretamente.' }
         format.json { render :show, status: :created, location: @foto }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class FotosController < ApplicationController
   def update
     respond_to do |format|
       if @foto.update(foto_params)
-        format.html { redirect_to @foto, notice: 'Foto was successfully updated.' }
+        format.html { redirect_to @foto, notice: 'A foto foi editada com sucesso.' }
         format.json { render :show, status: :ok, location: @foto }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class FotosController < ApplicationController
   def destroy
     @foto.destroy
     respond_to do |format|
-      format.html { redirect_to fotos_url, notice: 'Foto was successfully destroyed.' }
+      format.html { redirect_to fotos_url, notice: 'A foto foi excluída com sucesso.' }
       format.json { head :no_content }
     end
   end
--- app/models/foto.rb
+++ app/models/foto.rb
@@ -1,3 +1,5 @@
 class Foto < ActiveRecord::Base
+	validates :nome, presence: true
+	validates :retrato, presence: true
 	has_attached_file :retrato, :styles => {:medium => '300x300>', :thumb => '80x80#' }
 end
